cfb3cdefb47a24ea86729ee686f689980ac6ccbb
gee-cpu <80972154+gee-cpu@users.noreply.github.com>
2021-05-04 18:59:12 +0300

    updated

--- src/main/java/geeapp/gee/security/AuthenticationFilter.java
+++ src/main/java/geeapp/gee/security/AuthenticationFilter.java
@@ -3,15 +3,23 @@ package geeapp.gee.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import geeapp.gee.userdetailsrequestmodel.UserLogInModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Signature;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -21,11 +29,11 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     }
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req ,
-                                                HttpServletResponse)throws {
+                                                HttpServletResponse res)throws AuthenticationException {
         try {
             UserLogInModel creds=new ObjectMapper().
                     readValue(req.getInputStream(),UserLogInModel.class);
-            return AuthenticationManager.authenticate(
+            return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             creds.getEmail(),
                             creds.getPassword(),
@@ -35,5 +43,19 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         }
         catch (IOException e){ throw new RuntimeException(e);}
     }
+@Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth)throws IOException, ServletException {
+        String userName=((User) auth.getPrincipal()).getUsername();
+        //String tokenSecret=new SecurityConstant().getTokenSecret();
+    String token= Jwts.builder()
+            .setSubject(userName)
+            .setExpiration(new Date(System.currentTimeMillis()+SecurityConstants.EXPIRATION_TIME))
+            .signWith(SignatureAlgorithm.HS512,SecurityConstants.TOKEN_SECRET)
+            .compact();
+res.addHeader(SecurityConstants.HEADER_STRING,SecurityConstants.TOKEN_PREFIX +token);
 
+}
 }
--- src/main/java/geeapp/gee/security/SecurityConstants.java
+++ src/main/java/geeapp/gee/security/SecurityConstants.java
@@ -7,5 +7,5 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX="Bearer";
     public static final String HEADER_STRING= "Authorization";
     public static final String TOKEN_SECRET="jf9i4jgu83nfl0";
-    public static final String SIGN_UP_uRL="/user";
+    public static final String SIGN_UP_uRL="/User";
 }
